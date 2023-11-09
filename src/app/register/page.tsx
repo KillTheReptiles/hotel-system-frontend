655119b21f799d9f4d2ab1a4a04bfb5a2c838f6d
chemokita13 <chemokita13@gmail.com>
2023-11-09 21:12:52 +0100

    Add user registration page with form submission and error handling

---
+++ src/app/register/page.tsx
@@ -0,0 +1,73 @@
+'use client';
+import { useAuth } from '@/components/authContext';
+import { User } from '@/types/user';
+import { UserCredential } from 'firebase/auth';
+import React, { useState } from 'react';
+
+function Page() {
+    const authContext = useAuth();
+    const [UserForm, setUserForm] = useState<User>({
+        username: '',
+        password: '',
+    });
+    const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const response: UserCredential = await authContext.signUp(
+                UserForm.username,
+                UserForm.password
+            );
+            // if response
+            console.log(response);
+            alert(`User created with email: ${response.user.email}`);
+        } catch (Error: any) {
+            switch (Error.code) {
+                case 'auth/invalid-email':
+                    alert('Invalid email');
+                    break;
+                case 'auth/email-already-in-use':
+                    alert('Email already in use');
+                    break;
+                case 'auth/weak-password':
+                    alert('Weak password');
+                    break;
+                case 'auth/invalid-password':
+                    alert('Invalid password');
+                    break;
+                case 'auth/invalid-username':
+                    alert('Invalid username');
+                    break;
+                default:
+                    alert('Unknown error');
+                    break;
+            }
+        }
+    };
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const name = e.currentTarget.name;
+        const value = e.currentTarget.value;
+        setUserForm((prev) => ({ ...prev, [name]: value }));
+    };
+    return (
+        <div>
+            <form onSubmit={(e) => handleFormSubmit(e)}>
+                <input
+                    type='text'
+                    name='username'
+                    id='username'
+                    onChange={(e) => handleInputChange(e)}
+                />
+                <input
+                    type='text'
+                    name='password'
+                    id='password'
+                    typeof='password'
+                    onChange={(e) => handleInputChange(e)}
+                />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Page;
